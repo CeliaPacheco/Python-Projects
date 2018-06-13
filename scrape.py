0b12af4c8f834a54a9646b6f2e04462219ffd72e
celia <cpache00@nmt.edu>
2018-06-13 16:48:44 -0600

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.reddit.com/r/dailyprogrammer/'
+headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0'}
+response = requests.get(url, headers=headers)
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+
+
+#TODO: Get this working properly, right now it only prints one result
+links = {} 
+for link in soup.find_all('a'):
+    for h2 in link.find_all('h2'):
+        if "[Intermediate]" in str(h2):
+            #print(h2.prettify())
+            #print("++++++++++++++++++++")
+            #print(link.prettify())
+            links[h2] = link
+print(links)
