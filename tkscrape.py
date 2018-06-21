2cdac471f80ece46cfccbf5507988e128cefe1d6
celia <cpache00@nmt.edu>
2018-06-21 16:23:00 -0600

    Got window to display list of links

--- tkscrape.py
+++ tkscrape.py
@@ -10,20 +10,19 @@ class Scrape(tk.Tk):
         self.geometry("300x400")
 
         self.url_label = tk.Label(self, text="Enter URL here")
-        self.url_entry = tk.Text(self)
+        self.url_entry = tk.Entry(self)
+
+        for link in self.links:
+            link.pack()
 
         self.url_label.pack()
         self.url_entry.pack()
 
         self.bind("<Return>", self.open_url)
 
-        print(self.links)
 
-        for link in self.links:
-            link.pack()
-
-    def open_url(self):
-        url = self.url_entry.get(1.0, tk.END).strip()
+    def open_url(self, event):
+        url = self.url_entry.get().strip()
         print(url)
         #url = 'https://www.reddit.com/r/dailyprogrammer/'
         headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0'}
@@ -31,15 +30,17 @@ class Scrape(tk.Tk):
         html = response.content
         soup = BeautifulSoup(html, 'html.parser')
 
-
         self.get_soup(soup)
-#TODO this this part working correctly.
+        return
+
     def get_soup(self, soup):
         for link in soup.find_all('a'):
             for h2 in link.find_all('h2'):
                 if "[Intermediate]" in str(h2):
-                   self.links.append(link)
-
+                    #TODO work on displaying a hyperlinks
+                    new_link = tk.Label(self, text=str(link), pady=10)
+                    new_link.pack(side=tk.TOP, fill=tk.X)
+                    self.links.append(new_link)
 
 if __name__ == "__main__":
     scraper = Scrape()
