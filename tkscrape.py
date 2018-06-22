6d2bd4a3b84f5a571663b77ea94062dfdd30fea4
celia <cpache00@nmt.edu>
2018-06-22 16:34:55 -0600

    Got window to display something similar to a hyperlink. Need clean it up

--- tkscrape.py
+++ tkscrape.py
@@ -1,46 +1,66 @@
 import tkinter as tk
 import requests
+import webbrowser
 from bs4 import BeautifulSoup
 
 class Scrape(tk.Tk):
+    """This is the base class for the scraper"""
     def __init__(self):
         super().__init__()
-        self.links = []
+
+        self.links = {} #this dictionary holds both the title, url to found sites
         self.title("Scraper App")
-        self.geometry("300x400")
+        self.geometry("500x500")
 
-        self.url_label = tk.Label(self, text="Enter URL here")
-        self.url_entry = tk.Entry(self)
+        self.main_frame = tk.Frame(self, width=500, height=300)
 
-        for link in self.links:
-            link.pack()
+        self.url_label = tk.Label(self.main_frame, text="Enter URL here")
+        self.url_entry = tk.Entry(self.main_frame)
 
+        self.main_frame.pack()
         self.url_label.pack()
         self.url_entry.pack()
 
         self.bind("<Return>", self.open_url)
 
-
     def open_url(self, event):
+        """Open the entered url to begin scraping"""
         url = self.url_entry.get().strip()
-        print(url)
-        #url = 'https://www.reddit.com/r/dailyprogrammer/'
+        # This header is needed so reddit doesn't get angry
         headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0'}
+        # the actual opening of the url
         response = requests.get(url, headers=headers)
+        # get the html content
         html = response.content
+        self.get_links(html)
+
+    def get_links(self, html):
+        """Look though the html content for the wanted information"""
         soup = BeautifulSoup(html, 'html.parser')
-
-        self.get_soup(soup)
-        return
-
-    def get_soup(self, soup):
         for link in soup.find_all('a'):
             for h2 in link.find_all('h2'):
                 if "[Intermediate]" in str(h2):
-                    #TODO work on displaying a hyperlinks
-                    new_link = tk.Label(self, text=str(link), pady=10)
+                    # make a new entry with title and the link url
+                    self.links[str(h2)] = self.get_link(link)
+                    new_link = tk.Label(self, text=str(h2), fg="blue", cursor="hand2")
                     new_link.pack(side=tk.TOP, fill=tk.X)
-                    self.links.append(new_link)
+                    new_link.bind("<Button-1>", self.callback)
+
+    def get_link(self, link):
+        """Get the proper url for the required area"""
+        text = "https://www.reddit.com"
+        return text + link.get('href')
+
+    #TODO strip h2 to only get title
+    def get_title(self, h2):
+        pass
+
+    def callback(self, event):
+        """This will open the clicked on link"""
+
+        #THis line opens the clicked on link by looking up the text portion
+        #in the dictionary
+        webbrowser.open_new(self.links[event.widget.cget("text")])
 
 
 if __name__ == "__main__":
